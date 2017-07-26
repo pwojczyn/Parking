aef0478cd122468daf6800bc90a475f7c1749c16
pwojczyn <pwojczyn@gmail.com>
2017-07-26 13:45:32 +0200

    Update 1.11

--- src/com/pwojczyn/parking/Main.java
+++ src/com/pwojczyn/parking/Main.java
@@ -34,7 +34,8 @@ public class Main {
      }
 
     public static void addParking(){
-        //Parking(String parkingName, String parkingCity, String parkingStreet, int numberOfParkingPlace)
+        //Parking(String parkingName, String parkingCity, String parkingStreet, int numberOfParkingPlace);
+        
         System.out.println("Podaj nazwę parkingu: ");
         String parkingName=scanner.nextLine();
         System.out.println("Podaj miasto parkingu: ");
